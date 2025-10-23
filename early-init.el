7b2e996ae53ca6f9b7ef14fbd047131b095e006f
Matthew Kennedy <burnsidemk@gmail.com>
2025-10-23 14:26:26 -0500

    redo

--- early-init.el
+++ early-init.el
@@ -1,4 +1,6 @@
-(setq inhibit-startup-message t
-      inhibit-startup-screen t)
+;; -*- lexical-binding: t; -*-
 (setq package-enable-at-startup nil)
-(setq tool-bar-mode nil)
+(setq gc-cons-threshold 100000000)
+(setq read-process-output-max 1000000)
+(setq inhibit-startup-screen t)
+(tool-bar-mode -1)
--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;; -*- lexical-binding: t; indent-tabs-mode: nil -*-
+
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name
@@ -16,162 +18,98 @@
 
 (straight-use-package 'use-package)
 
+(use-package diminish :straight t)
+
 (use-package exec-path-from-shell
   :straight t
   :if (or (memq window-system '(mac ns x))
-	  (daemonp))
+          (daemonp))
   :config
   (exec-path-from-shell-initialize))
 
-(use-package diminish :straight t)
-
 (use-package which-key
+  :straight t
   :config (which-key-mode)
   :diminish)
 
+(use-package flycheck :straight t)
+
 (use-package company
   :straight t
-  :config
-  (global-company-mode))
+  :hook ((emacs-lisp-mode . company-mode)))
 
-(use-package company-quickhelp
-  :straight t)
-
-(use-package sly
-  :straight t
-  :after (company)
-  :custom
-  (sly-lisp-implementations
-   `((sbcl ("sbcl" "--dynamic-space-size" "4Gb") :coding-system utf-8-unix)
-     (clisp ("clisp" "-q") :coding-system utf-8-unix)
-     (ciel ("sbcl" "--core" ,(expand-file-name "~/Quicklisp/local-projects/CIEL/ciel-core") "--eval" "(in-package :ciel-user)"))))
-  (sly-default-lisp 'sbcl)
-  (sly-db-focus-debugger 'always))
-
-(use-package clojure-mode :straight t)
-(use-package cider :straight t)
-
-(add-to-list 'load-path "/Users/mkennedy/Library/Application Support/SuperCollider/downloaded-quarks/scel/el")
-(setq exec-path (append exec-path '("/Applications/SuperCollider.app/Contents/MacOS/")))
-
-(require 'sclang)
-
-;; (use-package clojure-mode
-;;   :straight t)
-
-;; (use-package cider
-;;   :straight t
-;;   :config (setq cider-repl-display-help-banner nil))
-
-(use-package magit
-  :straight t
-  :config
-  (setq magit-define-global-key-bindings 'recommended))
-
-(use-package docker
-  :straight t)
-
-(straight-use-package
- '(eat :type git
-       :host codeberg
-       :repo "akib/emacs-eat"
-       :files ("*.el" ("term" "term/*.el") "*.texi"
-               "*.ti" ("terminfo/e" "terminfo/e/*")
-               ("terminfo/65" "terminfo/65/*")
-               ("integration" "integration/*")
-               (:exclude ".dir-locals.el" "*-tests.el"))))
-
-(use-package verb
-  :straight t)
-
-(use-package org
-  :mode ("\\.org\\'" . org-mode)
-  :config (define-key org-mode-map (kbd "C-c C-r") verb-command-map))
-
-(use-package vterm
-  :straight t)
-
-(use-package julia-snail
-  :straight t
-  :custom (julia-snail-terminal-type :vterm)
-  :hook (julia-mode . julia-snail-mode))
-
-;; (use-package nano
-;;   :straight (:type git :host github :repo "rougier/nano-emacs"))
-
-;; (use-package ivy
-;;   :straight t
-;;   :config 
-;;   (ivy-mode)
-;;   (setopt ivy-use-virtual-buffers t)
-;;   (setopt enable-recursive-minibuffers t)
-;;   :diminish)
-
-(use-package projectile
-  :straight t
-  :config
-  (setopt projectile-project-search-path '(("~/Projects/" . 1)))
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-  (projectile-mode))
+(use-package company-quickhelp :straight t)
 
 (use-package lsp-mode
   :straight t
-  :hook ((lsp-mode . lsp-enable-which-key-integration))
-  :config
-  (setq lsp-enable-file-watchers nil
-        lsp-headerline-breadcrumb-enable nil)
-  ;; Performance tweaks, see
-  ;; https://github.com/emacs-lsp/lsp-mode#performance
-  (setq gc-cons-threshold 100000000)
-  (setq read-process-output-max (* 1024 1024)) ;; 1mb
-  (setq lsp-idle-delay 0.500))
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((java-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui
+  :straight t
+  :commands lsp-ui-mode)
+
+(use-package lsp-treemacs :straight t)
 
 (use-package lsp-java
   :straight t
   :config
-  (add-hook 'java-mode-hook 'lsp)
-  (setq lsp-java-vmargs
-        '("-noverify"
-          "-Xmx4G"
-          "-XX:+UseG1GC"
-          "-XX:+UseStringDeduplication"
-          "-javaagent:/Users/mkennedy/.m2/repository/org/projectlombok/lombok/1.18.36/lombok-1.18.36.jar")
-        lsp-java-java-path "/usr/lib/jvm/java-21-openjdk/bin/java"
+  (let ((jar (car (sort (directory-files-recursively
+                         (expand-file-name "~/.m2/repository/org/projectlombok/lombok/")
+                         ".*lombok\\(-[0-9]+.[0-9]+.[0-9]+\\)?.jar$")
+                        :reverse t))))
+    (if (and jar (file-exists-p jar))
+        (add-to-list 'lsp-java-vmargs (format "-javaagent:%s" jar) t)
+      (warn "Missing %s" jar))))
 
-        ;; Don't organise imports on save
-        lsp-java-save-action-organize-imports nil
-
-        ;; Don't format my source code (I use Maven for enforcing my
-        ;; coding style)
-        lsp-java-format-enabled nil))
+;; (use-package lsp-java-boot
+;;   :ensure nil
+;;   :hook ((lsp-mode . lsp-lens-mode)
+;;       (java-mode . lsp-java-boot-lens-mode)))
 
 (use-package dap-mode
   :straight t
   :after lsp-mode
+  :bind
+  (:map dap-mode-map
+        ("<f9>" . dap-continue)
+        ("<f7>" . dap-step-in)
+        ("<f8>" . dap-next))
   :config (dap-auto-configure-mode))
 
 (use-package dap-java :ensure nil)
 
-(use-package lsp-ui
+(use-package magit :straight t)
+
+(use-package editorconfig
+  :ensure nil
+  :config (editorconfig-mode 1))
+
+(use-package desktop
+  :ensure nil
+  :config (desktop-save-mode 1))
+
+(use-package simple
+  :ensure nil
+  :config (auto-save-mode))
+
+(use-package xt-mouse
+  :ensure nil
+  :config (xterm-mouse-mode 1))
+
+(use-package eat
   :straight t)
 
-(use-package shell-maker
-  :straight (:type git :host github :repo "xenodium/shell-maker"))
-
-(use-package chatgpt-shell
-  :straight (:type git :host github :repo "xenodium/chatgpt-shell"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ns-alternate-modifier 'meta)
- '(ns-command-modifier 'super)
- '(tool-bar-mode nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "JetBrains Mono" :foundry "nil" :slant normal :weight regular :height 130 :width normal)))))
+;; (use-package sly
+;;   :straight t
+;;   :after (company)
+;;   :custom
+;;   (sly-lisp-implementations
+;;    `((sbcl ("sbcl" "--dynamic-space-size" "4Gb") :coding-system utf-8-unix)
+;;      (clisp ("clisp" "-q") :coding-system utf-8-unix)
+;;      (ciel ("sbcl" "--core" ,(expand-file-name "~/Quicklisp/local-projects/CIEL/ciel-core") "--eval" "(in-package :ciel-user)"))))
+;;   (sly-default-lisp 'sbcl)
+;;   (sly-db-focus-debugger 'always))
